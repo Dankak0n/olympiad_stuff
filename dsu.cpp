0e097d179cd281bb50e93d3b32423d20e8b60438
Daniel Fon Kono <117820677+Dankak0n@users.noreply.github.com>
2023-08-30 17:27:07 +0300

    Create dsu.cpp

---
+++ dsu.cpp
@@ -0,0 +1,19 @@
+//copied from jiangly (https://codeforces.com/contest/1740/submission/178363447)
+struct DSU {
+	std::vector<int> f, siz;
+	DSU(int n) : f(n), siz(n, 1) { std::iota(f.begin(), f.end(), 0); }
+	int leader(int x) {
+		while (x != f[x]) x = f[x] = f[f[x]];
+		return x;
+	}
+	bool same(int x, int y) { return leader(x) == leader(y); }
+	bool merge(int x, int y) {
+		x = leader(x);
+		y = leader(y);
+		if (x == y) return false;
+		siz[x] += siz[y];
+		f[y] = x;
+		return true;
+	}
+	int size(int x) { return siz[leader(x)]; }
+};
