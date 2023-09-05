79cfb0de31377b19266b8fc9af1d9d0e7d2252d5
Daniel Fon Kono <117820677+Dankak0n@users.noreply.github.com>
2023-09-06 00:54:13 +0300

    Update indexed_set.cpp

--- indexed_set.cpp
+++ indexed_set.cpp
@@ -2,7 +2,7 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #define lower_than order_of_key
 namespace indexed_set {
-template<class T = int, class CMP = std::less<T>>
+template <class T = int, class CMP = std::less<T>>
 class iset : public __gnu_pbds::tree
 <T, __gnu_pbds::null_type, CMP, __gnu_pbds::rb_tree_tag,__gnu_pbds::tree_order_statistics_node_update>
 { public: T get(int pos) { return *(*this).find_by_order(pos); } };
