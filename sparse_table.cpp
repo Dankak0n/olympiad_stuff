7bcd937c00f9d46c9d307fc36c0516a7b1156655
Daniel Fon Kono <117820677+Dankak0n@users.noreply.github.com>
2023-11-05 12:26:50 +0300

    Update sparse_table.cpp

--- sparse_table.cpp
+++ sparse_table.cpp
@@ -1,35 +1,28 @@
 //sparse_table-----------------------------------------------------------------------------------------
 #include<cmath>
 #include<vector>
+#include<cassert>
 #include<functional>
 namespace sparse_table {
-template <class T> std::function<T(T&, T&)> _default_cmp = [](T &_l, T &_r){ return (_l < _r ? _l : _r); };
+template <class T> T _default_fun(T &_l, T &_r) { return (_l < _r ? _l : _r); };
 template <class T = int>
-class sparse_t {
-public:
-	std::vector<std::vector<T>> _st;
-	std::function<T(T&, T&)> _cmp_;
-	void build(const std::vector<T> &_v, const std::function<T(T&, T&)> _cmp = _default_cmp<T>) {
-		_cmp_ = _cmp;
-		_st.push_back(_v);
+struct sparse_t {
+	std::vector<std::vector<T> > _st;
+	std::function<T(T&, T&)> _fun;
+	void build(const std::vector<T> &_v, const std::function<T(T&, T&)> __fun = _default_fun<T>) {
+		_fun = __fun; _st.clear(); _st.push_back(_v);
 		for (int i = 1; !_st.back().empty(); i++) {
 			_st.push_back({});
-			for (int j = 0; j + (1 << i) <= (int)_v.size(); j++) {
-				_st.back().push_back(_cmp_(_st[i - 1][j],  _st[i - 1][j + (1 << (i - 1))]));
-			}
+			for (int j = 0; j + (1 << i) <= (int)_v.size(); j++)
+				_st.back().push_back(_fun(_st[i - 1][j],  _st[i - 1][j + (1 << (i - 1))]));
 		}
 	}
 	sparse_t(){};
-	sparse_t(const std::vector<T> &_v, const std::function<T(T&, T&)> _cmp = _default_cmp<T>) { sparse_table::sparse_t<T>::build(_v, _cmp); }
-	T get(int _l, int _r) {
-		int _sz = _r - _l + 1;
-		for (int i = log2(_sz) - 2;; i++) {
-			if (i >= 0 && _sz < (1 << (i + 1))) {
-				_sz = i;
-				break;
-			}
-		}
-		return (_cmp_(_st[_sz][_l],  _st[_sz][_r - (1 << _sz) + 1]));
+	sparse_t(const std::vector<T> &_v, const std::function<T(T&, T&)> __fun = _default_fun<T>) { build(_v, __fun); }
+	T get(int _l, int _r) {//[0, n-1]
+		assert(_l <= _r && 0 <= _l && _r < (int)_st[0].size());
+		int _sz = std::__lg(_r - _l + 1);
+		return _fun(_st[_sz][_l],  _st[_sz][_r - (1 << _sz) + 1]);
 	}
 };
 }
